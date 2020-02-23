1fbe546b20d5e2741631d610dff349a642d643f0
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-02-23 15:36:28 -0500

    n+1 everywhere, introduced a bug

--- main.cpp
+++ main.cpp
@@ -12,14 +12,14 @@ int main(int argc, const char **argv) {
     occa::json args = parseArgs(argc, argv);
 
     // These two should be input arguments
-    const unsigned int N = 256; 
+    const unsigned int N = 8; 
     const unsigned int levels = 4;
     const float tolerance = 1e-10;
 
     // Figuring out how many times we can coarsen
     unsigned int max_levels = 1;
     unsigned int N_at_h = 128;
-    while (N_at_h%2 == 0 && N_at_h > 4){
+    while (N_at_h%2 == 0 && N_at_h > 2){
         ++max_levels;
         N_at_h /= 2;
     }
@@ -34,40 +34,40 @@ int main(int argc, const char **argv) {
     }
 
     // Vector of u containing all the grids
-    std::vector<float> u(2*N, 1.0); // 1 is the initial guess
+    std::vector<float> u(2*(N+1), 1.0); // 1 is the initial guess
 
-    // Vector containg number of nodes for all levels
+    // Vector containg number of elements for all levels
     std::vector<float> N_h(max_levels, N);
 
     // Vector for keeping track of where all grids are in the big vector
     std::vector<float> offset(max_levels, 0.0);
 
     // Vector containing delta_x of each level
-    std::vector<float> delta_x(max_levels, 1.0/(N - 1));
+    std::vector<float> delta_x(max_levels, 1.0/N);
 
     // Vector containing f at each point for the finest grid
-    std::vector<float> f(N, 0.0);
+    std::vector<float> f(N+1, 0.0);
 
     // u_star is cached, so that it can be computed before the u_i start being updated
-    std::vector<float> u_star(2*N, 0.0);
+    std::vector<float> u_star(2*(N+1), 0.0);
 
     // Residuals are cached, so that it can run in parallel
-    std::vector<float> r(2*N, 0.0);
+    std::vector<float> r(2*(N+1), 0.0);
 
     for (unsigned int h = 1; h < max_levels; ++h) {
         N_h[h] /= intExp2(h);
-        offset[h] = offset[h-1] + N_h[h-1];
-        delta_x[h] = 1.0/(N_h[h] - 1); // square here?
+        offset[h] = offset[h-1] + N_h[h-1] + 1;
+        delta_x[h] = 1.0/N_h[h]; // square here?
     }
 
-    for (unsigned int i = 0; i < N; ++i) {
+    for (unsigned int i = 0; i <= N; ++i) {
         f[i] = std::pow(delta_x[0], 2) * std::pow(M_PI, 2) * std::sin(M_PI * i * delta_x[0]);
     }
 
     // Initial conditions
     for (unsigned int h = 0; h < max_levels; ++h) {
         u[offset[h]] = 0;           // u(0) = 0
-        u[offset[h]+N_h[h]-1] = 0;  // u(1) = 0
+        u[offset[h]+N_h[h]] = 0;    // u(1) = 0
     }
 
     // Jacobi iteration
@@ -79,10 +79,10 @@ int main(int argc, const char **argv) {
     auto t_start = std::chrono::high_resolution_clock::now();
     while (residual > tolerance) {
         ++n;
-        for (unsigned int i = 1; i < N-1; ++i) {
+        for (unsigned int i = 1; i < N; ++i) {
             u_star[offset[h] + i] = 0.5*(u[offset[h] + i + 1] + u[offset[h] + i - 1] + f[i]);
         }
-        for (unsigned int i = 1; i < N-1; ++i) {
+        for (unsigned int i = 1; i < N; ++i) {
             r[offset[h] + i] = weight * (u_star[offset[h] + i] - u[offset[h] + i]);
             u[offset[h] + i] += r[offset[h] + i];
         }
@@ -96,7 +96,7 @@ int main(int argc, const char **argv) {
 
         // Max
         residual = 0.0;
-        for (unsigned int i = 1; i < N-1; ++i) {
+        for (unsigned int i = 1; i < N; ++i) {
             residual = std::max(residual, std::abs(r[offset[h] + i]));
         }
     }
@@ -104,21 +104,21 @@ int main(int argc, const char **argv) {
 
     // Display section
     double error = 0.0;
-    for (unsigned int i = 1; i < N-1; ++i) {
+    for (unsigned int i = 1; i <= N; ++i) {
         error = std::max(error, std::abs(u[offset[0] + i] - std::sin(M_PI * i * delta_x[h])));
     }
 
-    std::cout << "i    numerical    analytical    residual    error" << std::endl;
-    for (unsigned int i = 0; i < N; ++i) {
-        std::cout << i << " " << std::setw(15) << u[offset[h] + i] << " " << std::setw(15) << r[offset[h] + i] << " " << std::setw(15) << std::sin(M_PI * i * delta_x[h]) << " " << std::setw(15) << std::abs(u[offset[h] + i] - std::sin(M_PI * i * delta_x[h])) << std::endl;
+    std::cout << "i      numerical      analytical        residual           error" << std::endl;
+    for (unsigned int i = 0; i <= N; ++i) {
+        std::cout << i << " " << std::setw(15) << u[offset[h] + i] << " " << std::setw(15) << std::sin(M_PI * i * delta_x[h]) << std::setw(15) << r[offset[h] + i] << " " << " " << std::setw(15) << std::abs(u[offset[h] + i] - std::sin(M_PI * i * delta_x[h])) << std::endl;
     }
 
-    std::cout << std::endl << "Iterations    max residual    max error    time taken [s]" << std::endl;
+    std::cout << std::endl << "Iterations  max residual   max error       time taken [s]" << std::endl;
     std::cout << n << " " << std::setw(15) << residual << " " << std::setw(15) << error << " " << std::setw(15) << std::chrono::duration<double, std::milli>(t_end-t_start).count()/1000.0 << std::endl;
 
 
 
-
+    /*
     int entries = 5;
 
     float *a  = new float[entries];
@@ -176,9 +176,9 @@ int main(int argc, const char **argv) {
     o_ab.copyTo(ab);
 
     // Assert values
-    /*for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < 5; ++i) {
         std::cout << i << ": " << ab[i] << '\n';
-    }*/
+    }
     for (int i = 0; i < entries; ++i) {
         if (ab[i] != (a[i] + b[i])) {
         throw 1;
@@ -189,6 +189,7 @@ int main(int argc, const char **argv) {
     delete [] a;
     delete [] b;
     delete [] ab;
+    */
 
     return 0;
 }
